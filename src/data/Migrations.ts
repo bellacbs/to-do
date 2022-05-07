8b8c82c8f6c3eff991b1144dd2ddefe68a3db74d
bellacbs <bellacbrandao@gmail.com>
2022-05-07 17:33:46 -0300

    :arrow_up: atualizando migrations

--- src/data/Migrations.ts
+++ src/data/Migrations.ts
@@ -16,8 +16,8 @@ export class Migrations extends BaseDataBase {
             id VARCHAR(255) PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             description VARCHAR(255) NOT NULL,
-            limit_date DATE NOT NULL,
-            limit_time TIME NOT NULL,
+            limit_date DATETIME NOT NULL,
+            edited_time DATETIME NOT NULL,
             creator_user_id VARCHAR(255) NOT NULL,
             FOREIGN KEY(creator_user_id) REFERENCES to_do_user(id)
         );
